f8a5089ac12d919b560322e3f11769da9eb5c2a5
AyubBodirkhonov <apple@Ayubs-MacBookAir.local>
2022-04-16 20:15:36 +0900

    Added my project

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import Navbar from './Navbar.js';
+import Home from './Home.js';
+import Create from './Create.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BlogDetails from './BlogDetails.js';
+import NotFound from './NotFound.js';
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/create">
+                <Create />
+              </Route>
+              <Route path="/blogs/:id">
+                <BlogDetails />
+              </Route>
+              <Route path="*">
+                <NotFound />
+              </Route>
+            </Switch>
+          </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
